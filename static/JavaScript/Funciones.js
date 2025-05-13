b31bb93a9a0416c392a5d1918e7970098900d082
El_Kike_PC1 <jsaaher.genius@gmail.com>
2025-05-13 09:30:54 +0200

    Primer commit de ejercicio-logica-1

---
+++ static/JavaScript/Funciones.js
@@ -0,0 +1,293 @@
+import { animals, countries, persons, pets } from "./datosEjercicio.js";
+export {
+  numAfectados,
+  numPersoasSanas,
+  numPersoasInfectadasPais,
+  nombreMascota,
+  datosPersonasInfectadas,
+  mayorPaisInfectado,
+  datosEspañolesPerro,
+  PersonasConMascota,
+  masCantidadMascota,
+  totalPatasMascota,
+  mascotaCuatroPatas,
+};
+
+//*Ejercicio 1*//
+
+function numAfectados() {
+  let numAfectados = 0;
+  for (let contador = 0; contador < persons.length; contador++) {
+    //console.log("persons[", contador, "]", persons[contador]);
+
+    if (persons[contador].infected) {
+      numAfectados++;
+    }
+  }
+
+  return numAfectados;
+}
+
+//*Ejercicio 2*//
+
+function numPersoasSanas() {
+  let numPersoasSanas = 0;
+  for (let contador = 0; contador < persons.length; contador++) {
+    //console.log("persons[", contador, "]", persons[contador]);
+
+    if (persons[contador].infected == false) {
+      numPersoasSanas++;
+    }
+  }
+
+  return numPersoasSanas;
+}
+
+//*Ejercicio 3*//
+
+function numPersoasInfectadasPais() {
+  let numPersoasInfectadasPorPais = 0;
+  for (let contador = 0; contador < countries.length; contador++) {
+    numPersoasInfectadasPorPais =
+      numPersoasInfectadasPorPais + countries[contador].infected;
+  }
+
+  return numPersoasInfectadasPorPais;
+}
+
+//*Ejercicio 4*//
+
+function mayorPaisInfectado() {
+  let paisesInfectados = 0;
+  let masInfectadoPais = "";
+  for (let paises of countries) {
+    if (paises.infected > paisesInfectados) {
+      paisesInfectados = paises.infected;
+      masInfectadoPais = paises.name;
+    }
+  }
+
+  return masInfectadoPais;
+}
+
+const paisConMasInfecciones = mayorPaisInfectado();
+console.log("País con más infectados:", paisConMasInfecciones);
+
+//*Ejercicio 5*//
+
+function nombreMascota() {
+  let nomMascotas = [];
+  let nombMascotas = "";
+  for (let { name } of pets) {
+    console.log(name);
+    nomMascotas.push(name);
+
+    nombMascotas = nomMascotas;
+  }
+  return nombMascotas;
+}
+//*Ejercicio 6*//
+
+function datosPersonasInfectadas() {
+  let datosPerInfect = [];
+  for (let persona of persons) {
+    if (persona.infected) datosPerInfect.push(persona);
+  }
+  return datosPerInfect;
+}
+
+console.log("Personas Infectadas", datosPersonasInfectadas);
+
+//*Ejercicio 7*//
+
+function datosEspañolesPerro() {
+  let españolesConPerro = [];
+  for (let personasEspañolas of persons) {
+    if (personasEspañolas.country === "ES") {
+      for (let mascota of pets) {
+        if (
+          mascota.name === personasEspañolas.pet &&
+          mascota.type === "perro"
+        ) {
+          españolesConPerro.push(personasEspañolas.name);
+        }
+      }
+    }
+  }
+  return españolesConPerro;
+}
+
+//*Ejercicio 8*//
+
+function PersonasConMascota() {
+  let personasMascota = [];
+  for (let persona of persons) {
+    for (let mascota of pets) {
+      if (mascota.name === persona.pet) {
+        personasMascota.push({
+          persona: persona.name,
+          mascota: {
+            nombre: mascota.name,
+            tipo: mascota.type,
+          },
+        });
+      }
+    }
+  }
+  return personasMascota;
+}
+
+//*Ejercicio 9*//
+function masCantidadMascota() {
+  let conteoTipos = [];
+  /* creando un objeto de tipo, cantidad
+  [tipo, numero]*/
+
+  for (let persona of persons) {
+    for (let mascotas of pets) {
+      if (persona.name === mascotas.name) {
+        let tipo = pet.type;
+        let encontrado = false;
+
+        /* Bucle que recorre a cada persona, aqui estoy creando la variable tipo que permite
+          sacar el tipo de animal usando el array de personas*/
+
+        /* Inicializo encontrado para hacer un !encontrado, se utiliza por si en el futuro
+          se quiere agregar otro tipo de animal, lo que esta haciendo es saber si ya contamos
+          el tipo de mascota, es decir, si ya existe, entonces = false, por lo que no tiene
+          que hacer el conteo que viene a continuacion */
+
+        for (
+          let contadorTipos = 0;
+          contadorTipos < conteoTipos.length;
+          contadorTipos++
+        ) {
+          if (conteoTipos(contadorTipos).tipo === tipo) {
+            conteoTipos(contadorTipos).cantidad++;
+            encontrado = true;
+          }
+        }
+
+        if (!encontrado) {
+          conteoTipos.push({
+            tipo: tipo,
+            cantidad: 1,
+          });
+        }
+      }
+
+      /*  se tiene la condicion if(!encontrado), que lo que dice es que si conteoTipos.tipo
+    es igual a tipo que es pet.type en el bucle anterior, entonces hara un objeto llamado
+    conteoTipos con forma [tipo, cantidad], en donde si el valor tipo es igual al valor
+    tipo creado en el bucle anterior, sumara una unidad (++), al tener un
+    elemento ! hay que inicializarlo con un false y despues un true o viceversa. */
+
+      /* Ahora buscamos en ese objeto nuevo, en el valor de cantidad, cual es mayor */
+    }
+  }
+
+  let mayorTipo = "";
+  let mayorCantidadTipos = 0;
+  for (let tipo of conteoTipos) {
+    if (tipo.cantidad > mayorCantidadTipos) {
+      mayorCantidadTipos = tipo.cantidad;
+      mayorTipo = tipo.tipo;
+    }
+  }
+  return mayorTipo;
+}
+
+//*Ejercicio 10*//
+
+function totalPatasMascota() {
+  let conteoTipo = [];
+
+  for (let persona of persons) {
+    for (let mascota of pets) {
+      if (persona.pet === mascota.name) {
+        let tipo = mascota.type;
+        let encontrado = false;
+
+        for (
+          let contadorTipos = 0;
+          contadorTipos < conteoTipo.length;
+          contadorTipos++
+        ) {
+          if (conteoTipo[contadorTipos].tipo === tipo) {
+            conteoTipo[contadorTipos].cantidad++;
+            encontrado = true;
+          }
+        }
+
+        if (!encontrado) {
+          conteoTipo.push({ tipo: tipo, cantidad: 1 });
+        }
+      }
+    }
+  }
+  let totalPatas = 0;
+
+  for (let patas of conteoTipo) {
+    for (let animal of animals) {
+      if (animal.kind === patas.tipo) {
+        totalPatas += patas.cantidad * animal.legs;
+      }
+    }
+  }
+
+  return totalPatas;
+}
+/** 
+ MANERA MAS FACIL
+
+ function totalPatasMascota() {
+  let totalPatas = 0;
+
+  for (let persona of persons) {
+    // Encontrar qué mascota tiene esta persona
+    for (let mascota of pets) {
+      if (persona.pet === mascota.name) {
+        let tipoMascota = mascota.type;
+
+        // Buscar cuántas patas tiene ese tipo de animal
+        for (let animal of animals) {
+          if (animal.kind === tipoMascota) {
+            totalPatas += animal.legs;
+          }
+        }
+      }
+    }
+  }
+
+  return totalPatas; */
+
+//*Ejercicio 11*//
+function mascotaCuatroPatas() {
+  let nombrePersona = [];
+  for(let persona of persons){
+    for(let mascota of pets){
+      if (persona.pet === mascota.name){
+        let tipo = mascota.type;
+        /* let nombreMascota = persona.name; */
+
+        for(let animales of animals){
+          if(tipo === animales.kind){
+            if(animales.legs === 4){
+              nombrePersona.push(persona.name);
+
+            }
+
+          }
+
+        }
+      }
+
+    }
+  }
+  return nombrePersona
+}
+
+
+//*Ejercicio 12*//
+//*Ejercicio 13*//
+//*Ejercicio 14*//
---
+++ static/JavaScript/Index.js
@@ -0,0 +1,70 @@
+import {
+  numAfectados,
+  numPersoasInfectadasPais,
+  numPersoasSanas,
+  nombreMascota,
+  datosPersonasInfectadas,
+  mayorPaisInfectado,
+  datosEspañolesPerro,
+  PersonasConMascota,
+  masCantidadMascota,
+  totalPatasMascota,
+  mascotaCuatroPatas,
+  
+} from "./Funciones.js";
+
+const numAfectadosExer1 = numAfectados();
+console.log("Exercicio 1: Número de persoas infectadas ", numAfectadosExer1);
+
+const numPersoasSanasExer2 = numPersoasSanas();
+console.log(
+  "Exercicio 2: Número de persoas non infectadas ",
+  numPersoasSanasExer2
+);
+
+const numPersoasInfectadasPorPais = numPersoasInfectadasPais();
+console.log(
+  "Exercicio 3: Número de persoas infectadas nos países do array ",
+  numPersoasInfectadasPorPais
+);
+
+const paisMasInfectadoEjercicio4 = mayorPaisInfectado();
+console.log("Ejercicio 4: Mayor Pais Infectado", paisMasInfectadoEjercicio4);
+
+const nombreMascotaEjercicio5 = nombreMascota();
+console.log("Ejercicio 5: Nombre de las mascotas", nombreMascotaEjercicio5);
+
+const datosPersonasInfectadasEjercicio6 = datosPersonasInfectadas();
+console.log(
+  "Ejercicio 6: Personas Infectadas",
+  datosPersonasInfectadasEjercicio6
+);
+
+const EspañolesPerroEjercicio7 = datosEspañolesPerro();
+console.log(
+  "Ejercicio 7: personas españolas con perro",
+  EspañolesPerroEjercicio7
+);
+
+const PersonasYMascotaEjercicio8 = PersonasConMascota();
+console.log(
+  "Ejercicio 8: personas con los datos de su mascota:",
+  PersonasYMascotaEjercicio8
+);
+
+const masPersonasMascotaEjercicio9 = masCantidadMascota();
+console.log(
+  "Ejercicio 9: el animal que mas personas tienen es",
+  masPersonasMascotaEjercicio9
+);
+
+const todasPatasMascotas = totalPatasMascota();
+console.log(
+  "Ejercicio 10: el numero total de patas de las mascotas es: ",
+  todasPatasMascotas
+);
+
+const personasAnimalesCuatroPatas = mascotaCuatroPatas();
+console.log(
+  "Ejercicio 11: las personas que tienen mascotas que tienen 4 patas son: ", personasAnimalesCuatroPatas
+);
